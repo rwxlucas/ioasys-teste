3ff33372d1fd5900c0fd5addf14fbb3101c365a5
Lucas Faria <lucasfjb.dev@gmail.com>
2021-11-08 23:18:15 -0300

    wip

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,38 +2,14 @@ import { Schema, Document, model } from "mongoose";
 import { IUserModel } from "../utils/types";
 
 const userSchema = new Schema<IUserModel>({
-	name: {
-		type: String,
-		required: true
-	},
-	birthday: {
-		type: String,
-		required: true
-	},
-	UF: {
-		type: String,
-		required: true
-	},
-	password: {
-		type: String,
-		required: true
-	},
-	city: {
-		type: String,
-		required: true
-	},
-	schooling: {
-		type: String,
-		required: true
-	},
-	email: {
-		type: String,
-		required: true
-	},
-	role: {
-		type: [String],
-		required: true
-	}
+	name: { type: String, required: true },
+	birthday: { type: String, required: true },
+	UF: { type: String, required: true },
+	password: { type: String, required: true },
+	city: { type: String, required: true },
+	schooling: { type: String, required: true },
+	email: { type: String, required: true },
+	role: { type: [String], required: true }
 });
 
 const userModel = model<IUserModel>('user', userSchema);
